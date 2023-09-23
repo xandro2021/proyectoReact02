444b929ea1fbd98843fcff23cf53c194b3d0a9bb
xandro2021 <xandrocastro2021@gmail.com>
2023-09-23 12:53:34 -0600

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import Header from './components/Header';
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
 
   return (
-    <>
-      <h1>Control de Gastos</h1>
-    </>
-  )
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
+    </div>
+  );
 }
 
 export default App
