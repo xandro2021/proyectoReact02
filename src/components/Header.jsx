f76934d5e9555787313d146c1cb258780318a9a2
xandro2021 <xandrocastro2021@gmail.com>
2023-09-23 17:07:02 -0600

    Display the form is the value is invalid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,28 @@
 import NuevoPresupuesto from './NuevoPresupuesto';
 
-const Header = ({ presupuesto, setPresupuesto }) => {
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto
+}) => {
+
   return (
     <header>
+
       <h1>Planificador de Gastos</h1>
-      <NuevoPresupuesto
-        presupuesto={presupuesto}
-        setPresupuesto={setPresupuesto}
-      />
+
+      {isValidPresupuesto ? (
+        <p> Control Presupuesto </p>
+
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+
     </header>
   );
 };
