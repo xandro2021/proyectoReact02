cafce051de4f5182ff780b473a8b59ca36501f91
xandro2021 <xandrocastro2021@gmail.com>
2023-09-24 12:24:09 -0600

    Crear componente Modal y controlarlo con un State

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import Header from './components/Header';
+import Modal from './components/Modal';
 import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  };
 
   return (
     <div>
@@ -17,9 +23,15 @@ function App() {
 
       {isValidPresupuesto && (
         <div className="nuevo-gasto">
-          <img alt="icono nuevo gasto" src={IconoNuevoGasto} />
+          <img
+            alt="icono nuevo gasto"
+            src={IconoNuevoGasto}
+            onClick={handleNuevoGasto}
+          />
         </div>
       )}
+
+      {modal && <Modal setModal={setModal} />}
 
     </div>
   );
