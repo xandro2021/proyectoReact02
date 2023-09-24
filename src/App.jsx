9994dbec25d6fd7d502859745d58618506b9e382
xandro2021 <xandrocastro2021@gmail.com>
2023-09-24 17:43:06 -0600

    Agregar formulario sencillo al modal

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg';
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
   const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
 
   const handleNuevoGasto = () => {
     setModal(true);
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 200)
   };
 
   return (
@@ -31,7 +37,11 @@ function App() {
         </div>
       )}
 
-      {modal && <Modal setModal={setModal} />}
+      {modal && <Modal
+                  setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                />}
 
     </div>
   );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,71 @@
 import CerrarBtn from '../img/cerrar.svg';
 
-const Modal = ({ setModal }) => {
+const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
   const ocultarModal = () => {
-    setModal(false);
+    setAnimarModal(false);
+
+    setTimeout(() => {
+      setModal(false);
+    }, 500)
   };
 
   return (
     <div className="modal">
+
       <div className="cerrar-modal">
         <img
           alt="cerrar modal"
           src={CerrarBtn}
-          onClick={ ocultarModal }
+          onClick={ocultarModal}
         />
       </div>
+
+      <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+        <legend>Nuevo Gasto</legend>
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Agrega el nombre del gasto"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Agrega la cantidad del gasto: ej. 300"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+
+          <select id="" name="">
+            <option value=""> -- Seleccione -- </option>
+            <option value="ahorro"> Ahorro </option>
+            <option value="comida"> Comida </option>
+            <option value="casa"> Casa </option>
+            <option value="gastos"> Gastos </option>
+            <option value="ocio"> Ocio </option>
+            <option value="salud"> Salud </option>
+            <option value="suscripciones"> Suscripciones </option>
+          </select>
+
+        </div>
+
+        <input
+          type="submit"
+          value="Agregar gasto"
+        />
+      </form>
+
     </div>
   );
 };
