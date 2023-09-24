b70db8f949c3a2a43a88bf604857f8f97fb2f4a2
xandro2021 <xandrocastro2021@gmail.com>
2023-09-23 18:12:30 -0600

    Importar icono para agregar gasto (abrir modal)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header';
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
